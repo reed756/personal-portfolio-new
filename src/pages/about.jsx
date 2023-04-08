f3da3a2aa1b62d8d10e1013b440b6125e0a76c24
reedanj <james.r@unitaskr.com>
2023-04-08 10:31:40 +0100

    initial commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,134 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/portrait.jpg'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+        target="_blank"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About - James Reed</title>
+        <meta
+          name="description"
+          content="I’m James Reed. I live in London, where I develop apps for ios, android and web."
+        />
+      </Head>
+      <Container className="mt-16 sm:mt-32">
+        <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+          <div className="lg:pl-20">
+            <div className="max-w-xs px-2.5 lg:max-w-none">
+              <Image
+                src={portraitImage}
+                alt=""
+                sizes="(min-width: 1024px) 32rem, 20rem"
+                className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+              />
+            </div>
+          </div>
+          <div className="lg:order-first lg:row-span-2">
+            <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+              I’m James Reed. I live in London, where I develop apps for mobile
+              and web.
+            </h1>
+            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+              <p>
+                I made my start in coding through curriculums at FreeCodeCamp
+                and The Odin Project. Following this I entered a coding bootcamp
+                by Northcoders in November 2021 My dedication and hard work paid
+                off as I successfully completed a Full Stack software
+                development bootcamp at Northcoders in March 2022.
+              </p>
+              <p>
+                Currently, I am a Junior Software Developer at UniTaskr, where I
+                am leveraging my skills to build innovative software solutions.
+                During my time there I have helped build the UI for a brand new
+                app which serves a user base of over 250,000 students and
+                businesses. Following this, I have built the UI for the apps
+                premium subscription model which businesses can sign up to to
+                post jobs and tasks.
+              </p>
+              <p>
+                Outside of work, I enjoy Muay Thai training, reading, going to
+                local music gigs, playing chess, and watching sports.
+              </p>
+              <p>
+                With a strong drive to continuously learn and grow, I am excited
+                for the opportunities to make an impact in the field of software
+                development.
+              </p>
+            </div>
+          </div>
+          <div className="lg:pl-20">
+            <ul role="list">
+              <SocialLink
+                href="https://twitter.com/reedandj1"
+                icon={TwitterIcon}
+              >
+                Follow on Twitter
+              </SocialLink>
+              {/* <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+                Follow on Instagram
+              </SocialLink> */}
+              <SocialLink
+                href="https://github.com/reed756"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
+                Follow on GitHub
+              </SocialLink>
+              <SocialLink
+                href="https://www.linkedin.com/in/james-reed-a6b413111/"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
+                Follow on LinkedIn
+              </SocialLink>
+              <SocialLink
+                href="mailto:reedandj1@gmail.com"
+                icon={MailIcon}
+                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+              >
+                reedandj1@gmail.com
+              </SocialLink>
+            </ul>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+}
